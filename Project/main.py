fbc50bd3a3082a56e03f7accc36aece9a9edde64
Lucasw54 <100497443+Lucasw54@users.noreply.github.com>
2022-05-23 14:49:01 -0400

    Create Window

--- Project/main.py
+++ Project/main.py
@@ -1,1 +1,16 @@
-Put python code here
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((1400, 800))
+pygame.display.set_caption('Physics Environment')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('Fonts/OpenSans-Bold.ttf', 50)
+game_active = True
+start_time = 0
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
