1c285faa3416727e961ffe78c8cfb274f0be03b5
Lucasw54 <100497443+Lucasw54@users.noreply.github.com>
2022-05-24 14:51:15 -0400

    Scenario Layout
    Began formatting the game loop

--- Project/main.py
+++ Project/main.py
@@ -2,15 +2,38 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((1400, 800))
+screen = pygame.display.set_mode((1200, 700))
 pygame.display.set_caption('Physics Environment')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('Fonts/OpenSans-Bold.ttf', 50)
-game_active = True
+ScenarioOne_active = False
+ScenarioTwo_active = False
+ScenarioThree_active = False
+ScenarioSelect_active = True
 start_time = 0
+
+groundScenarioOne_surf = pygame.image.load('Graphics/Ground.png').convert_alpha()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    if ScenarioSelect_active:
+        print("Scenario Select")
+    else:
+        if ScenarioOne_active:
+            screen.blit(groundScenarioOne_surf, (0, 300))
+            print("Scenario 1")
+
+        if ScenarioTwo_active:
+            screen.blit(groundScenarioOne_surf, (0, 400))  # change to Scenario two ground
+            print("Scenario 2")
+
+        if ScenarioThree_active:
+            screen.blit(groundScenarioOne_surf, (0, 500))  # change to Scenario three ground
+            print("Scenario 3")
+
+    pygame.display.update()
+    clock.tick(60)
